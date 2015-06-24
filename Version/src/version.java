eac2affef7c1a68e118f0cce68170c9132809c95
Peter <peter77613@yahoo.com.tw>
2015-06-24 11:04:05 +0800

    加入Customer,Product,Dao 檔案
    包括Bean,Service,DAO

--- Version/src/version.java
+++ Version/src/version.java
@@ -2,7 +2,9 @@
 public class version {
 
 	public static void main(String[] args) {
-		System.out.println("Version");
+		System.out.println("Version1");
+		
+		System.out.println("Version2");
 	}
 
 }
