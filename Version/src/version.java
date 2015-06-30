d1cfc9563ac1dc5fa8fa2a2019bd60f38743a2a9
Peter <peter77613@yahoo.com.tw>
2015-06-30 11:37:04 +0800

    122

--- Version/src/version.java
+++ Version/src/version.java
@@ -3,7 +3,7 @@ public class version {
 
 	public static void main(String[] args) {
 		System.out.println("Version1");
-		
+		System.out.println("Version1");
 		System.out.println("Version2");
 		
 		System.out.println("Version3");
